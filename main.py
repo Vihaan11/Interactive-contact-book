205cabe8ad1ad2d7be1381b422aaf990e7186b1a
Vihaan11 <76782237+Vihaan11@users.noreply.github.com>
2024-05-25 10:01:32 +0000

    Added del_contact function

--- main.py
+++ main.py
@@ -3,3 +3,23 @@ numbers=[]
 def add_contact(name, number):
   contact.append(name)
   numbers.append(number)
+
+
+def del_contact():
+   contact_to_del=input("Which contact do you want to delete (Enter name of contact : case sensitive): ")
+   index_no = -1
+   
+   for i in contact:
+      index_no += 1
+      
+      if i == contact_to_del:
+         contact.pop(index_no)
+         numbers.pop(index_no)
+         break
+         
+      elif (index_no+1) > len(contact):
+         print("Contact not found")
+         break
+         
+      else:
+         continue
